1e998f3fd324e0a5feb463dabea8f0d492cf53ff
Drainfort <ja_romerom@msn.com>
2013-02-07 13:54:34 -0500

    aqui ya cree la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = 'f727576d432b23fddd625d414dfb805a444f325644376a5e955b66ae8f9e495fb92def48433cb25beb98e116c0432fa4f5da45772b59bdbf644b0825c3d73820'
